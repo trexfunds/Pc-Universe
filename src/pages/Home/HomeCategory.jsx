0c53a9aac945aae2ec6fbd857bc52492a62d169f
Kushal Chaudhari <trexfunds07@gmail.com>
2023-11-06 21:17:10 +0530

    category images /

--- src/pages/Home/HomeCategory.jsx
+++ src/pages/Home/HomeCategory.jsx
@@ -7,37 +7,37 @@ const btnText = "Get Started Now";
 
 const categoryList = [
     {
-        imgUrl: 'src/assets/images/category/01.png',
+        imgUrl: '/src/assets/images/category/01.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'GIGABYTE',
     },
     {
-        imgUrl: 'src/assets/images/category/02.png',
+        imgUrl: '/src/assets/images/category/02.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'MSI',
     },
     {
-        imgUrl: 'src/assets/images/category/03.png',
+        imgUrl: '/src/assets/images/category/03.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'VR Ready',
     },
     {
-        imgUrl: 'src/assets/images/category/04.png',
+        imgUrl: '/src/assets/images/category/04.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'GTX',
     },
     {
-        imgUrl: 'src/assets/images/category/05.png',
+        imgUrl: '/src/assets/images/category/05.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'GTX 1050Ti',
     },
     {
-        imgUrl: 'src/assets/images/category/06.png',
+        imgUrl: '/src/assets/images/category/06.png',
         imgAlt: 'category rajibraj91 rajibraj',
         iconName: 'icofont-brand-windows',
         title: 'GeForce',
